4a06aaa7774e9f1fbce3d3edc0c77172e28bb90e
showdae <showdae3721@naver.com>
2023-07-19 06:06:49 +0900

    23071906

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,15 @@
 // git push - 23071717
 // git push - 23071804
+// git push - 23071906
  
-#include <iostream>	// 입출력
+// 표준 라이브러리
+#include <iostream>	// 콘솔 스트림
+#include <fstream>	// 파일 스트림
+#include <sstream>	// 문자열 스트림
 #include <cstring>	// C 문자열 스타일 (API)
 #include <string>	// C++ 문자열 스타일 (클래스)
 #include <cassert>	// 예외 처리
+#include <iomanip>	// 포맷 조작자를 사용하기 위한 헤더 파일
 
 using namespace std;
 
@@ -124,7 +129,6 @@ using namespace std;
  }
 
 
-#include <iomanip> // 포맷 조작자를 사용하기 위한 헤더 파일
  // 템플릿 함수 변형 (배열)
  // N 값은 컴파일러에 의해 "추론"되거나 "명시적"으로 전달 받음
  template <typename T, int N>
@@ -141,14 +145,154 @@ using namespace std;
 int main()
 {
 	///////// chapter16: 입출력 스트림 /////////
+	// 스트림이란: 프로그램과 데이터소스 or 데이터싱크의 연결 다리(자동 변환)
+	// 데이터소스: 키보드, 파일, 문자열
+	// 데이터싱크: 모니터, 파일, 문자열
+	// 데이터소스(Byte) -> 입력스트림(자료형 변환) -> PG -> 출력스트림(Byte열 변환) -> 데이터싱크(Byte)
+	// iostream: istream(입력), ostream(출력) 상속받아 iostream(입출력) 사용: stringStream(문자열), fStream(파일)
 
+	///// 1. 콘솔 스트림 (일시적인 형태, 키보드 -> 모니터) /////
+	// 헤더: <iostream>
+	// 사용 클래스: istream, ostream, iostream
+	// 사용 목적: 키보드와 콘솔 프로그램에 입출력할때 사용
+	int iNum;
+	cin >> iNum; // 데이터소스 10 입력
+	// 프로그램 메모리: 0x0000003831cffb64 [0a 00 00 00]
+	cout << iNum << endl << endl; // 데이터싱크 10 출력
 
+	///// 2. 파일 스트림 (영구적인 형태, csv -> csv) /////
+	// 헤더: <fstream>
+	// 사용 클래스: ifstream, ofstream, fstream
+	// 사용 목적: 파일과 프로그램을 연결해서 파일을 읽고 쓸때 사용
+	// 파일이란:		8비트 바이트열, 파일에서 데이터를 읽고 쓸때는 스트림 객체를 활용
+	//				스트림 객체가 파일을 읽는다면, 시스템에 의해서 파일의 내용이 대량으로 버퍼에 복사됨
+	//				스트림 객체가 파일을 쓸때도, 시스템에 의해서 버퍼에서 파일로 데이터가 대량으로 전달됨
+	// 스트림 종류:	파일 생성: ofstream, 파일 읽기 ifstream, 수정 fstream
+	// 마커:			파일의 바이트 위치를 가리킴 (현재 마커 위치 리턴: inStream tellg(), outStream tellp())
+	// 순차 접근:	마커가 버퍼에 순차적으로 접근
+	// 임의 접근:	마커가 특정 위치에 곧바로 접근
+
+	ofstream outStream; // 출력 스트림 객체 인스턴스화
+	outStream.open("test1.csv"); // 파일 열기 (확장자명으로 열기)
+	//outStream.open("integerFile.csv", ios::out | ios::app); // 열기 모드: 파일 뒤에 추가할때
+	if (!outStream.is_open()) // 파일이 제대로 열렸는지 확인
+	{ cout << "파일 생성 실패" << endl; }
+
+	for (int i = 0; i < 3; i++)
+	{
+		outStream << i + 1;		// 파일에 값 쓰기
+		if (i != 2) {
+			outStream << ",";	// 쉼표로 구분
+		}
+	}
+	outStream.close(); // 파일 연결 끊기 (파일 스트림은 한 번에 하나의 파일만 연결)
+
+	ifstream inStream;
+	inStream.open("test1.csv");
+	if (!inStream.is_open())
+	{ cout << "파일 읽기 실패" << endl; }
+	
+	string line;
+	getline(inStream, line); // 한 줄씩 읽기
+
+	stringstream ss(line);
+	string token;
+
+	while (getline(ss, token, ',')) {	// 쉼표를 구분자로 사용하여 숫자 추출
+		int number = stoi(token);		// stoi(): 문자열을 정수로 변환하는 함수
+		cout << number << ",";
+	}
+	cout << endl;
+	inStream.close(); // 파일 연결 끊기
+
+	///// 3. 이진 파일 스트림 (이진데이터 -> 이진데이터) /////
+	// 텍스트 파일이 아닌 데이터를 처리할때 사용 (이미지 파일..._
+	int iNum1 = 12345; double dNim1 = 12.345;
+
+	ofstream binaryOut;
+	binaryOut.open("test2.txt", ios::out | ios::binary); // 열기모드: out 출력목적, binary 이진
+	if (!binaryOut.is_open())
+	{ cout << "파일 생성 실패" << endl; }
+
+	// write(): sizeof()개의 글자를 배열에서 스트림 버퍼로 출력
+	// int 변수 주소를 받고 char 포인터로 변환한뒤 파일에 쓰기
+	binaryOut.write(reinterpret_cast<char*>(&iNum1), sizeof(int));
+	binaryOut.write(reinterpret_cast<char*>(&dNim1), sizeof(double));
+	binaryOut.close();
+
+	int iNum2; double dNum2;
+
+	ifstream binaryIn;
+	binaryIn.open("test2.txt", ios::in, ios::binary); // 엵기모드: in 입력목적, binary 이진
+	if (!binaryIn.is_open())
+	{ cout << "파일 읽기 실패" << endl; }
+
+	// read(): 스트림 버퍼에서 sizeof()개의 글자를 읽고, Num2에 문자 배열을 채움
+	// int 변수 주소를 기반으로 메모리에 예약되어 있는 위치를 char 포인터로 변환한뒤 파일 읽기
+	binaryIn.read(reinterpret_cast<char*>(&iNum2), sizeof(int));
+	binaryIn.read(reinterpret_cast<char*>(&dNum2), sizeof(double));
+	binaryIn.close();
+
+	cout << "iNum2: " << iNum2 << endl;
+	cout << "dNum2: " << dNum2 << endl;
+
+	///// 4. 문자열 스트림 (문자열 -> 문자열) /////
+	// 헤더: <sstream>
+	// 사용 클래스: istringstream, ostringstream, stringstream
+	// 사용 목적: 문자열에서 데이터를 읽어 들이거나, 문자열로 데이터를 출력할때 사용
+	// 콘솔스트림, 파일스트림은 데이터 소스와 싱크가 프로그램 외부에 있지만, 문자열 스트림은 내부에 있다
+	// 데이터 소스와 싱크가 내부에 있으므로 열고 닫을 필요가 없다 (open, close 함수가 없다)
+	// 어댑터: stringstream 객체를 어댑터로 사용하여 형변환
+	// 랩: 다른 자료형 -> 문자열 자료형으로 변환 (ostringstream)
+	// 언랩: 문자열 자료형 -> 다른 자료형으로 변환 (istringstream)
+
+	stringstream strStream("AAA AAA");	// 생성자 초기화
+	cout << strStream.str() << endl;
+	strStream.str("BBB BBB");			// str(): 매개변수를 호스트객체와 연결
+	cout << strStream.str() << endl;	// str(): 호스트 객체에 연결된 문자열 리턴
+
+	///// 5. 데이터 형식화 (조정자) /////
+	// 헤더: <iomanip>
+	// 사용 클래스: ios
+	// 사용 목적: 입출력 형태 조정
+	// 형식화 플래그(fmtFlag):	ios 클래스는 7개의 값이 조합된 fmtFlag라는 자료형을 정의
+	//							fmtFlags::setf(flag): 플래그 설정, fmtFlags::unsetf(flag): 플래그 해제 
+	// 형식화 필드:				ios 클래스는 3개의 필드 그룹을 제공
+	//							fmtField::setf(addingField, field): 필드 설정, fmtField::unsetf(field): 필드 해제 
+	// 형식화 변수:				width, precision, fill
+	//							int ios::width(int n), int ios::precision(int n), int ios::fill(char c)
+	
+	bool b = true;
+	double d = 12.3456;
+	bool b2 = true;
+	double d2 = 12.3456;
+
+	// 플래그, 필드, 변수를 사용한 형식화
+	cout << "형식화 없이 출력" << endl;
+	cout << "b: " << b << endl;
+	cout << "d: " << d << endl << endl;
+
+	cout << "bool 형식화" << endl;
+	cout.setf(ios::boolalpha);				// 형식화 플래그: 불 자료형을 true or false 형태로 출력
+	cout << "b: " << b << endl << endl;
+
+	cout << "double 형식화" << endl;
+	cout.setf(ios::fixed, ios::floatfield);	// 형식화 필드: 부동 소수점을 고정 표기법으로 출력
+	cout.width(10);							// 형식화 변수: 출력할 필드의 크기 지정
+	cout.precision(1);						// 형식화 변수: 소수점 아래의 몇 자리까지 출력할지 지정
+	cout.fill('*');							// 형식화 변수: 사용되지 않는 공간을 채우기 위한 문자 지정
+	cout << "d: " << d << endl << endl;
+
+	// 표준 조정자를 사용한 형식화 (<iomanip> 헤더를 읽어야 사용할 수 있음)
+	cout << "표준 조정자 사용" << endl;
+	cout << "b2: " << boolalpha << b2 << endl;
+	cout << "d2: " << showpoint << right << fixed << setw(16) << setprecision(2)
+		 << setfill('*') << d2 << endl;
 
 
 
 	return 0; // main 함수에서 문제가 없으면 0을 리턴, 문제가 있으면 다른 값을 리턴
 	///////////////////////////////////////
-
 
 
 	/*
@@ -287,6 +431,7 @@ int main()
 
 	/*
 	///////// chapter11: 클래스의 관계 /////////
+	// Parent, Child 클래스 참조
 	// 상속: classB에서 classA의 멤버 변수를 상속
 	// classA		classB : public classA
 		  
--- Test.cpp
+++ Test.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 // 스마트 포인터 ?
 // ifndef, define, endif 전처리기 ?
+// 버퍼란?
+// 콜백 함수란?
+// eof 비트?
 
-int main()
-{
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
 
 	return 0;
 }
